f6598b5380f70afac94265017396756aa663ca88
Khang <92634153+HoangKhang5207@users.noreply.github.com>
2023-09-12 11:18:22 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Tiêu đề web",
+    introTitle: 'NGUYỄN KIỀU PHƯƠNG',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^Ấn vào ây ii^^',
+    title: 'Phải chăng EM đã yêu ngay từ cái nhìn đầu tiên 🥰',
+    desc: 'Phải chăng EM đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Thích nhắm <3',
+    btnNo: 'Hông nha :>',
+    question: 'Trên thế giới có hơn 7 tỉ người mà sao EM lại yêu anh <3',
+    btnReply: 'Gửi anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Anh biết mà 🥰. Yêu EM nhiều lắm 😘😘',
+    messDesc: 'EM có yêu anh hong thì bảo :>',
+    btnAccept: 'Okee lun <3',
+    messLink: 'https://www.facebook.com/profile.php?id=100022597170543' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
